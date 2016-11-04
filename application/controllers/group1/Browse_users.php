3251b77a6191dc7489371f9fe363984dff0020f4
Nick Beaudoin <believewithyoureyes@gmail.com>
2016-11-04 19:17:09 -0400

    db

--- application/controllers/group1/Browse_users.php
+++ application/controllers/group1/Browse_users.php
@@ -1,19 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Browse_users extends CI_Controller {
-    public $clearance = '1111'; // maybe place this in the constructor -> set to permission number in sessions
-	
-	public function __construct(){
-        parent::__construct(); 
+class Browse_users extends CI_Controller
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
         $this->load->model('group1_models/user_model'); // I can now access the User_model class ($this->User_model)
     }
 
 
-    public function index() {
+    public function index()
+    {
         // add if statement 
         // if admin continue
-        if($this->session->logged_in){ // if logged in show browse users
+        if ($this->session->logged_in) { // if logged in show browse users
 
             $data['user_array'] = (array)$this->user_model->filterUsers($this->input->post());
 
@@ -21,74 +23,77 @@ class Browse_users extends CI_Controller {
             $this->load->view('group1/templates/header');
             $this->load->view('group1/templates/navbar/navbar');
             $this->load->view('group1/browse/user_table', $data);
-            $this->load->view('group1/templates/navbar/navbottom'); 
+            $this->load->view('group1/templates/navbar/navbottom');
             $this->load->view('group1/templates/footer');
 
-		} else { // else redirect to Login_controller
+        } else { // else redirect to Login_controller
 
-			header('Location: login');
-	
-		}
-        
+            header('Location: login');
+
+        }
+
     }
 
 
-
-    public function userProfile($userID = NULL){
+    public function userProfile($userID = NULL)
+    {
         // 'Details' button on user_dash
-        
-        if($this->session->logged_in){ // if logged in show user profile
 
-            $user_id = $userID != NULL? $userID:$this->input->post('user_id');
+        if ($this->session->logged_in) { // if logged in show user profile
+
+            $user_id = $userID != NULL ? $userID : $this->input->post('user_id');
 
             $data['user_array'] = (array)$this->user_model->getAllUserInfo($user_id);// get all user info based on posted user_id
             $data['current_user_clearance'] = str_split($this->user_model->getPermission($this->session->userID));
-            
+
             $this->load->view('group1/templates/header');
             $this->load->view('group1/templates/navbar/navbar');
             $this->load->view('group1/browse/user_profile', $data); // load user_profile page
-            $this->load->view('group1/templates/navbar/navbottom'); 
+            $this->load->view('group1/templates/navbar/navbottom');
             $this->load->view('group1/templates/footer');
 
-		} else { // else redirect to Login_controller
+        } else { // else redirect to Login_controller
 
-			header('Location: login');
+            header('Location: login');
 
-		}
+        }
     }
 
 
     // displays the Edit User page for admin users
-    public function editAccount(){
+    public function editAccount()
+    {
         // new page with pre-populated user registration form
         $this->load->helper('form');
-        
+
         $userID = $this->input->post('user_id');
-        
-        if($this->session->logged_in){ // if logged in show user profile
+
+        if ($this->session->logged_in) { // if logged in show user profile
 
             $data = (array)$this->user_model->getAllUserInfo($userID);// get all user info based on posted user_id
             $data['registration_edit'] = TRUE;
 
-            
+
             $this->load->view('group1/templates/header');
             $this->load->view('group1/templates/navbar/navbar');
             $this->load->view('group1/registration/registration_main', $data); // load registration page
-            $this->load->view('group1/templates/navbar/navbottom'); 
+            $this->load->view('group1/templates/navbar/navbottom');
             $this->load->view('group1/templates/footer');
 
-		} else { // else redirect to Login_controller
+        } else { // else redirect to Login_controller
 
-			header('Location: login');
-	
-		}
+            header('Location: login');
+
+        }
 
     }
+
     // update user after admin edit
-    public function updateUser(){
-        
-         $this->user_model->updateUserInfo($this->input->post());
-         $this->userProfile($this->input->post('user_id'));
+    public function updateUser()
+    {
+
+        $this->user_model->updateUserInfo($this->input->post());
+        $this->userProfile($this->input->post('user_id'));
     }
 
     // displays the change password page for non-admin users
@@ -119,27 +124,28 @@ class Browse_users extends CI_Controller {
 		}
     }
 
-    public function confirmUserPassword($error = FALSE){
+    public function confirmUserPassword($error = FALSE)
+    {
         $data['user_id'] = $this->input->post('user_id');
 
-        if($this->session->logged_in){ // if logged in show user profile
-         
-            if($this->input->post('doit') == 1){
-          
+        if ($this->session->logged_in) { // if logged in show user profile
 
-                //if password entered == admin password 
-                if(md5($this->input->post('password')) == $this->user_model->getPassword($this->session->userID)){
+            if ($this->input->post('doit') == 1) {
+
+
+                //if password entered == admin password
+                if (md5($this->input->post('password')) == $this->user_model->getPassword($this->session->userID)) {
                     // delete user
-                    
+
                     $this->user_model->deleteUser($this->input->post('user_id'));
 
-                    header('Location: '.base_url());
+                    header('Location: ' . base_url());
 
                 } else { // else redirect back to same page with red alert
-                    $_POST['doit'] = 0;    
+                    $_POST['doit'] = 0;
                     $this->confirmUserPassword(TRUE);
                 }
-                
+
 
             } else {
                 $data['error'] = $error;
@@ -150,15 +156,12 @@ class Browse_users extends CI_Controller {
                 $this->load->view('group1/templates/footer');
             }
 
-		} else { // else redirect to Login_controller
+        } else { // else redirect to Login_controller
 
-			header('Location: login');
-	
-		}
+            header('Location: login');
 
-        
+        }
+
+
     }
-
 }
-
-
--- application/views/group1/browse/user_profile.php
+++ application/views/group1/browse/user_profile.php
@@ -1,98 +1,99 @@
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?php echo $user_array['first_name'] . ' ' . $user_array['middle_initial'] . ' ' . $user_array['last_name'] . '\'s Profile' ?></h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class=" col-md-9 col-lg-9 ">
+                <table class="table table-user-information">
+                    <tbody>
+                    <tr>
+                        <td>User ID:</td>
+                        <td><?php echo $user_array['user_id'] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Status:</td>
+                        <td>
+                            <?php
 
-    <div class="panel panel-info">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?php echo $user_array['first_name'].' '.$user_array['middle_initial'].' '.$user_array['last_name'].'\'s Profile' ?></h3>
-        </div>
-        <div class="panel-body">
-            <div class="row">
-                <div class=" col-md-9 col-lg-9 ">
-                    <table class="table table-user-information">
-                        <tbody>
-                        <tr>
-                            <td>User ID:</td>
-                            <td><?php echo $user_array['user_id'] ?></td>
-                        </tr>
-                        <tr>
-                            <td>Status:</td>
-                            <td>
-                                <?php 
-                    
-                                    $permissionArray = str_split($user_array['permission']); 
-                                    if($permissionArray[0] == 1){
-                                        echo 'Administrator<br>';
-                                    }
-                                    if($permissionArray[1] == 1){
-                                        echo 'Teacher<br>';
-                                    }
-                                    if($permissionArray[2] == 1){
-                                        echo 'Parent<br>';
-                                    }
-                                    if($permissionArray[3]){
-                                        echo 'Student';
-                                    }
+                            $permissionArray = str_split($user_array['permission']);
+                            if ($permissionArray[0] == 1) {
+                                echo 'Administrator<br>';
+                            }
+                            if ($permissionArray[1] == 1) {
+                                echo 'Teacher<br>';
+                            }
+                            if ($permissionArray[2] == 1) {
+                                echo 'Parent<br>';
+                            }
+                            if ($permissionArray[3]) {
+                                echo 'Student';
+                            }
 
-                                ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Gender:</td>
-                            <td><?php echo $user_array['gender']? 'Female':'Male'; ?></td>
-                        </tr>
-                        <tr>
-                            <td>Email:</td>
-                            <td><?php echo $user_array['email'] ?></td>
-                        </tr>
-                        <tr>
-                            <td>Date of Birth</td>
-                            <td><?php echo date_format(date_create($user_array['birth_date']), 'M d, Y') ?></td>
-                        </tr>
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Gender:</td>
+                        <td><?php echo $user_array['gender'] ? 'Female' : 'Male'; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><?php echo $user_array['email'] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth</td>
+                        <td><?php echo date_format(date_create($user_array['birth_date']), 'M d, Y') ?></td>
+                    </tr>
 
-                        <tr>
-                        <tr>
-                            <td>Address</td>
-                            <td><?php echo $user_array['street'].', '.$user_array['city'].', '.$user_array['state'].' '.$user_array['zip'] ?></td>
-                        </tr>
-                        <tr>
+                    <tr>
+                    <tr>
+                        <td>Address</td>
+                        <td><?php echo $user_array['street'] . ', ' . $user_array['city'] . ', ' . $user_array['state'] . ' ' . $user_array['zip'] ?></td>
+                    </tr>
+                    <tr>
                         <td>Phone Number 1<br><br>Phone Number 2</td>
-                        <td><?php echo $user_array['phone_number_1'] ?><br><br><?php echo $user_array['phone_number_2'] ?></td>
-                        </tr>
-                        <tr>
-                            <td>Assigned Duty ID's:</td>
-                            <td><?php echo $user_array['misc_duties'] ?></td>
-                        </tr>
-                        <tr>
-                            <td>Notes:</td>
-                            <td><?php echo $user_array['notes'] ?></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                        <td><?php echo $user_array['phone_number_1'] ?>
+                            <br><br><?php echo $user_array['phone_number_2'] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Assigned Duty ID's:</td>
+                        <td><?php echo $user_array['misc_duties'] ?></td>
+                    </tr>
+                    <tr>
+                        <td>Notes:</td>
+                        <td><?php echo $user_array['notes'] ?></td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="panel-footer">
-            <!-- if administrator show 'edit account' button -->
-            <?php if($current_user_clearance[0]): ?>
-                <form action="./edit_account" method="post" style="display: inline-block">
-                    
-                    <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
-                    <button class="btn btn-warning">Edit Account</button>
-                    
-                </form>
-                <form action="./confirm_user_password" method="post" style="display: inline-block" onsubmit="return confirm('Are you sure you want to delete this user?');">
-                    
-                    <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
-                    <button class="btn btn-danger">Delete Account</button>
-                    
-                </form>
-            <?php endif; ?>
-            <!-- else show 'change password' button -->
-            
-                <form action="./change_password" method="post" style="display: inline-block">
-                    
-                    <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
-                    <button class="btn btn-info">Change Password</button>
-                    
-                </form>
-
-        </div>
     </div>
+    <div class="panel-footer">
+        <!-- if administrator show 'edit account' button -->
+        <?php if ($current_user_clearance[0]): ?>
+            <form action="./edit_account" method="post" style="display: inline-block">
+
+                <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
+                <button class="btn btn-warning">Edit Account</button>
+
+            </form>
+            <form action="./confirm_user_password" method="post" style="display: inline-block"
+                  onsubmit="return confirm('Are you sure you want to delete this user?');">
+
+                <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
+                <button class="btn btn-danger">Delete Account</button>
+
+            </form>
+        <?php endif; ?>
+        <!-- else show 'change password' button -->
+
+        <form action="./change_password" method="post" style="display: inline-block">
+
+            <input type="hidden" name="user_id" value="<?php echo $user_array['user_id'] ?>">
+            <button class="btn btn-info">Change Password</button>
+
+        </form>
+
+    </div>
+</div>
